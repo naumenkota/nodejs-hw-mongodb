34d61ba62e934d68ae89e787e8b2f6538ec74443
Tetiana Naumenko <naumenko.ta@gmail.com>
2025-07-01 13:12:07 +0300

    fix model

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,6 +1,6 @@
 import createHttpError from 'http-errors';
-import { SessionCollection } from '../db/models/session';
-import { UserCollection } from '../db/models/user';
+import { SessionCollection } from '../db/models/session.js';
+import { UserCollection } from '../db/models/user.js';
 
 export const authenticate = async (req, res, next) => {
     const authHeader = req.get('Authorization');
